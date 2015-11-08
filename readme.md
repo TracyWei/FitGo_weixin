d97aa0ffa0671abb521a5f2804e08d27da2caf5b
LJ <1948448114@qq.com>
2015-11-08 23:22:40 +0800

    基本框架搭建好像完成了

---
+++ readme.md
@@ -0,0 +1,27 @@
+##HeraldApp
+****
+###Function:
+
+#####Html5 APP producted by Herald.  
+#####We want to learn something at the same time doing something for others.  
+#####what we want to:  
+****
+ 1.yuyue  
+ 2.losts and found  
+ 3.query school life information
+
+#####we want to do more!
+
+###Installation
+****
+#####1.node is needed
+#####2.express is nedded(version is 4.13.1)
+	npm install -g express-generator@4
+#####3.the fore-end is spa(single-page application),we are going to use ionic(based on angularjs).so it's a challenge for us because we are not familiar with it.
+#####4.To deal with the problem of Cross-domain,we use nodejs to forward requests.Use
+	node server.js
+#####to start the nodejs server(listen on 3000)
+#####5.we want to use gulp in the future.
+****
+###end
+
---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true }));// for parsing application/x-www-form-urlencoded
+
+
+app.get('/api', function (req, res) {
+    // res.json(req.body)
+  res.send('Hello World!');
+  console.log('query'+JSON.stringify(req.query));
+  console.log('body'+JSON.stringify(req.body));
+  console.log('params:'+JSON.stringify(req.params));
+});
+app.post('/api',function(req,res){
+    res.send('Hello World!');
+    console.log('query'+JSON.stringify(req.query));
+    console.log('body'+JSON.stringify(req.body));
+    console.log('params:'+JSON.stringify(req.params));
+})
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
