a0156c5e131c8134c50a4d2c59a3b1413123c5d4
LJ <1948448114@qq.com>
2015-11-17 16:17:30 +0800

    ee

---
+++ js/mod/directive.js
@@ -0,0 +1,21 @@
+angular.module('HeraldApp')
+.directive('helloWorld', function(){
+    // Runs during compile
+    return {
+        // name: '',
+        // priority: 1,
+        // terminal: true,
+        // scope: {}, // {} = isolate, true = child, false/undefined = no change
+        // controller: function($scope, $element, $attrs, $transclude) {},
+        // require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+        restrict: 'AE', // E = Element, A = Attribute, C = Class, M = Comment
+        template: '<h3>hello world</h3>',
+        // templateUrl: '',
+        replae: true
+        // transclude: true,
+        // compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+        // link: function($scope, iElm, iAttrs, controller) {
+            
+        // }
+    };
+});
