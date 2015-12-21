318330ca0066d6d0b55064d67ce902940ad540ca
TracyWei <tracy_wei7@163.com>
2015-12-21 18:07:14 +0800

    完善个人信息

--- pages/user/user.js
+++ pages/user/user.js
@@ -1,9 +1,32 @@
 angular.module('FitGoApp')
 .controller('LoginCtrl', ['$scope', function($scope) {
+        var seconds={
+                    'seconds':60,
+                    'forbid':false,
+                    'getShow':true,
+                    'codeShow':false,
+                    'isForbid':true
+                }
+        $scope.seconds=seconds;
+        var timetoggle=function(){
+            seconds.getShow=!seconds.getShow;
+            seconds.codeShow=!seconds.codeShow;
+            seconds.forbid=!seconds.forbid;
+            seconds.isForbid=!seconds.isForbid;
+        }
         $scope.getcode=function(){
             console.log("getcode");
-            alert("hell")
+            seconds.forbid=true;
+            timetoggle();
+            var counter=setInterval(function(){
+            $scope.seconds.seconds--;              
+            if(seconds.seconds<=0){
+                timetoggle();
+                clearInterval(counter);
+                seconds.seconds=60;
+            }
+            $scope.$digest()
+        },1000);
         }
-        $scope.test = 2000;
-        // $scope.tests=[1,2,3];
     }])
+;
