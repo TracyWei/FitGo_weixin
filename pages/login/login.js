d6be7ec43d2838222924070ddb6e33eb26156ca0
TracyWei <tracy_wei7@163.com>
2015-12-23 20:31:41 +0800

    忘记密码的输入检查完成

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,76 +1,75 @@
 angular.module('FitGoApp')
-	.controller('loginController',['$scope', '$state', function($scope,$state) {
+	.controller('loginController', ['$scope', '$state', function($scope, $state) {
 		var words = {
 				'login_word': "手机登陆",
 				'verify_word': "验证"
 			}
-// ——————————————登录检查——————————————
+			// ——————————————登录检查——————————————
 		var notice = {
 			'p_null': false,
 			'pwd_null': false,
-			'pwd_6':false,
-			'auth': false,
+			'pwd_6': false,
+			'auth': false,//用户名密码正确性检查
 			'p_number': '',
 			'pwd_number': '',
 			'error_class': 1,
-			// 'flag':false
+			'flag6': false
 		}
 		$scope.notice = notice;
 		$scope.words = words;
 		$scope.plogin = function() {
-				console.log(notice.p_number);
+				//手机号不为空
 				if (!notice.p_number) {
 					notice.p_null = true;
 				} else {
 					notice.p_null = false;
-					if(notice.pwd_number.length<6)
-					{
-						notice.pwd_6=true;
-					}
-					else{
-						notice.pwd_6=false;
-					}
 				}
-				if (!notice.pwd_number) {
+				//密码不为空且大等于6
+				if (!notice.pwd_number || notice.pwd_number.length < 6) {
 					notice.pwd_null = true;
 				} else {
 					notice.pwd_null = false;
+					notice.flag6 = true;
+
 				}
-				if (notice.p_number && notice.pwd_number && !notice.auth) {
+				//按钮变化(输入都不为空，密码大等于6，用户名密码正确性未检查)
+				if (notice.p_number && notice.pwd_number && !notice.auth && notice.flag6) {
 					notice.error_class = 1;
 					words.login_word = "手机登陆";
-					// notice.flag=false;
 				} else {
 					notice.error_class = 0;
 					words.login_word = "出错啦⊙△⊙";
-					// notice.flag=true;	
 				}
 			}
-// ——————————————学号一卡通检查——————————————
+			// ——————————————学号一卡通检查——————————————
 		var vnotice = {
 			's_null': false,
 			'c_null': false,
-			'vauth': false,
+			'vauth': false,//一卡通学号正确
 			's_number': '',
 			'c_number': '',
 			'error_class': 1,
 		}
 		$scope.vnotice = vnotice;
 		$scope.verify = function() {
+				//学号为空
 				if (!vnotice.s_number) {
 					vnotice.s_null = true;
 				} else {
 					vnotice.s_null = false;
 				}
+				//一卡通为空
 				if (!vnotice.c_number) {
 					vnotice.c_null = true;
 				} else {
 					vnotice.c_null = false;
 				}
+				//按钮变化(输入均不为空，一卡通学号正确未检查)
 				if (vnotice.s_number && vnotice.c_number && !vnotice.vauth) {
 					notice.error_class = 1;
 					words.verify_word = "验证";
-					$state.go('e-signup')
+					$state.go('e-signup');
+
 				} else {
 					notice.error_class = 0;
 					words.verify_word = "出错啦⊙△⊙";
@@ -78,6 +77,12 @@ angular.module('FitGoApp')
 				}
 			}
 	}])
+
+
+
+
+
+
 
 
 
--- pages/user/user.js
+++ pages/user/user.js
@@ -28,5 +28,113 @@ angular.module('FitGoApp')
             $scope.$digest()
         },1000);
         }
+        var change_notice={
+            'p_null':false,
+            'code_null':false,
+            'pwd1_null':false,
+            'pwd_notsame':false,
+            'pwd2_null':false,
+            // 'pwd2_notsame':false,
+            'p_number':'',
+            'code_number':'',
+            'pwd1_number':'',
+            'pwd2_number':'',
+            'error_class':1,
+            'codeauth':false,//code正确性检查
+            'flag61':false,
+            'flag62':false
+        }
+        var words={
+            'change_word':"修改密码"
+        }
+        $scope.change_notice=change_notice;
+        $scope.words=words;
+        $scope.changepwd=function(){
+            //手机号码为空
+            if(!change_notice.p_number){
+                change_notice.p_null=true;
+            }
+            else{
+                change_notice.p_null=false;
+            }
+            //验证码为空
+            if(!change_notice.code_number){
+                change_notice.code_null=true;
+            }
+            else{
+                change_notice.code_null=false;
+            }
+            //密码为空或长度小于6
+            if(!change_notice.pwd1_number||change_notice.pwd1_number.length<6){
+                change_notice.pwd1_null=true;
+            }
+            else{
+                change_notice.pwd1_null=false;
+                change_notice.flag61=true;
+            }
+            //确认密码为空或长度小于6
+            if(!change_notice.pwd2_number||change_notice.pwd2_number.length<6){
+                change_notice.pwd2_null=true;
+            }
+            else{
+                change_notice.pwd2_null=false;
+                change_notice.flag62=true;
+                if(change_notice.pwd1_number==change_notice.pwd2_number){
+                    change_notice.pwd_notsame=false;
+                    console.log("change_notice.pwd2_notsame"+change_notice.pwd_notsame);
+                }
+                else
+                    change_notice.pwd_notsame=true;
+                    console.log("change_notice.pwd2_notsame"+change_notice.pwd_notsame);
+
+            }
+            //按钮变化(输入都不为空，密码长度大于等于6，两次输入一直，还未检查code正确性)
+            if (change_notice.p_number && change_notice.pwd1_number &&change_notice.pwd2_number 
+                && !change_notice.codeauth && change_notice.flag61&& change_notice.flag62
+                && change_notice.code_number&& !change_notice.pwd_notsame) 
+                {
+                    change_notice.error_class = 1;
+                    words.change_word = "修改密码";
+                    // notice.flag=false;
+                } 
+            else {
+                    change_notice.error_class = 0;
+                    words.change_word = "出错啦⊙△⊙";
+                    console.log("error")
+                    // notice.flag=true;    
+                }
+
+        }
     }])
 ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
