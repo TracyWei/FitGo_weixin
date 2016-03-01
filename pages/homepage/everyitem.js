ca680c59b24d3ef8294218a0735efeb850e94c40
TracyWei <tracy_wei7@163.com>
2016-03-01 23:28:06 +0800

    约我的差调个颜色

--- pages/homepage/everyitem.js
+++ pages/homepage/everyitem.js
@@ -7,6 +7,29 @@ angular.module('FitGoApp')
 			endTime:"2016-3-1-上午12:00",
 			remark:"想找个跑的慢一点的"
 		}
+		var everyOne={
+			name:"小A",
+			words:"hello!我们一起跑步吧!",
+			className:"ion-female",
+			isFemale:true,
+			isMale:false,
+			isNot:false,
+			itemClass:true,
+			agreeColor:false,
+			disagreeColor:false,
+			forbidden:false
+		}
 		$scope.displayItem=displayItem;
+		$scope.everyOne=everyOne;
 		console.log(displayItem.name);
+		$scope.agree=function(){
+			everyOne.agreeColor=true;
+			everyOne.forbidden=true;
+			console.log("shdhsf");
+		}
+		$scope.disagree=function(){
+			everyOne.disagreeColor=true;
+			everyOne.forbidden=true;
+			console.log("aaa");
+		}
 	}]);
